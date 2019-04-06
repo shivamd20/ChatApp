95dc046a31513247ec507e7a2eec99f66b79c1c1
Shivam Dwivedi <shivam.dwivedi@neptune-ubi.com>
2019-04-07 01:01:58 +0530

    learned ngxs

---
+++ src/app/read/read.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { Tutorial } from '../models/tutorial.model';
+import { Observable } from 'rxjs';
+import { Store } from '@ngxs/store';
+import { RemoveTutorial } from '../actions/tutorial.actions';
+
+@Component({
+    selector: 'app-read',
+    templateUrl: './read.component.html',
+    styleUrls: ['./read.component.css']
+})
+export class ReadComponent implements OnInit {
+    tutorials$: Observable<Tutorial>;
+
+    constructor(private store: Store) {
+        this.tutorials$ = this.store.select(state => state.tutorials.tutorials)
+    }
+
+    delTutorial(name) {
+        this.store.dispatch(new RemoveTutorial(name))
+    }
+
+    ngOnInit() { }
+}
