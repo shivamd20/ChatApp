0981af0d2918e521aef424deed6ab3a246f44e47
Shivam Dwivedi <shivam.dwivedi@neptune-ubi.com>
2019-04-07 15:42:58 +0530

    routing added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,24 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { CreateComponent } from './create/create.component';
+
+
+const routes: Routes = [
+    {
+        path: 'authenticate',
+        component: CreateComponent
+    }
+];
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+    declarations: [],
+    imports: [
+        CommonModule,
+        RouterModule.forRoot(routes)
+    ],
+    exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+
+}
