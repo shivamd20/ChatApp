e36377872f0049d5b2352081a845a673693df3c6
Shivam Dwivedi <shivam.dwivedi@neptune-ubi.com>
2019-04-19 17:24:07 +0530

    temp

--- src/app/modules/app/components/temp/temp.component.ts
+++ src/app/modules/app/components/temp/temp.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Apollo } from 'apollo-angular';
+import { Observable } from 'rxjs';
+import gql from 'graphql-tag';
 @Component({
-  selector: 'app-temp',
-  templateUrl: './temp.component.html',
-  styleUrls: ['./temp.component.css']
+    selector: 'app-temp',
+    templateUrl: './temp.component.html',
+    styleUrls: ['./temp.component.css']
 })
 export class TempComponent implements OnInit {
 
-  constructor() { }
+    constructor(private apollo: Apollo) { }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
+
+    getGames(): Observable<any> {
+        return this.apollo.subscribe({
+            query: gql`subscription($key: uuid){
+            ttt_state{
+              data
+              key
+            }
+          }`
+        },
+        );
+    }
 
 }
--- src/app/modules/routing/app-routing.module.ts
+++ src/app/modules/routing/app-routing.module.ts
@@ -2,12 +2,17 @@ import { NgModule, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthenticateComponent } from '../app/components/authenticate/authenticate.component';
+import { TempComponent } from '../app/components/temp/temp.component';
 
 
 const routes: Routes = [
     {
         path: 'authorize',
         component: AuthenticateComponent
+    },
+    {
+        path: 'temp',
+        component: TempComponent
     },
     {
         path: '**',
