339342bb5a56ef4d26364f95f204599e9c9d3c6e
Shivam Dwivedi <shivam.dwivedi@neptune-ubi.com>
2019-04-20 00:07:22 +0530

    chhota mota

--- src/app/modules/app/components/contacts/contacts.component.ts
+++ src/app/modules/app/components/contacts/contacts.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngxs/store';
 
 export interface Section {
     name: string;
@@ -29,10 +30,46 @@ export class ContactsComponent implements OnInit {
             'profile_pic': 'https://lh6.googleusercontent.com/-cpGv6Fv3rDU/AAAAAAAAAAI/AAAAAAAAFHY/i3FVMRazoyI/photo.jpg',
             'user_id': 'google-oauth2|105340382558189294995',
             'date_joined': '2019-04-19T17:44:28.922496+00:00'
+        }, {
+            'name': 'ramesh',
+            'profile_pic': 'ahaha',
+            'user_id': 'ramesh1',
+            'date_joined': '2019-04-19T12:44:38.911809+00:00'
+        },
+        {
+            'name': 'ramesh',
+            'profile_pic': 'profile_pic',
+            'user_id': 'user_id',
+            'date_joined': '2019-04-19T13:52:59.897708+00:00'
+        },
+        {
+            'name': 'Shivam Dwivedi',
+            'profile_pic': 'https://lh6.googleusercontent.com/-cpGv6Fv3rDU/AAAAAAAAAAI/AAAAAAAAFHY/i3FVMRazoyI/photo.jpg',
+            'user_id': 'google-oauth2|105340382558189294995',
+            'date_joined': '2019-04-19T17:44:28.922496+00:00'
+        }, {
+            'name': 'ramesh',
+            'profile_pic': 'ahaha',
+            'user_id': 'ramesh1',
+            'date_joined': '2019-04-19T12:44:38.911809+00:00'
+        },
+        {
+            'name': 'ramesh',
+            'profile_pic': 'profile_pic',
+            'user_id': 'user_id',
+            'date_joined': '2019-04-19T13:52:59.897708+00:00'
+        },
+        {
+            'name': 'Shivam Dwivedi',
+            'profile_pic': 'https://lh6.googleusercontent.com/-cpGv6Fv3rDU/AAAAAAAAAAI/AAAAAAAAFHY/i3FVMRazoyI/photo.jpg',
+            'user_id': 'google-oauth2|105340382558189294995',
+            'date_joined': '2019-04-19T17:44:28.922496+00:00'
         }
     ];
 
-    constructor() { }
+    constructor(private store: Store) {
+
+    }
 
     ngOnInit() {
     }
